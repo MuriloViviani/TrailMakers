8ca97aaebd5fb5455693b8eff0122106a10023c7
murilo viviani <muriloviviani@hotmail.com>
2017-10-12 20:10:29 -0300

    Save and load text methods implemented

---
+++ TrailMakers/TrailMakers.Android/DependencyServices/ISaveAndLoad_android.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using TrailMakers.Interface;
+using System.IO;
+
+namespace TrailMakers.Droid.DependencyServices
+{
+    public class ISaveAndLoad_Android : ISaveAndLoad
+    {
+        #region INTERFACE_IMPLEMENTATION
+        public bool CheckFile(string filename)
+        {
+            return File.Exists(PathToFile(filename));
+        }
+
+        public async Task<string> LoadTextAsync(string filename)
+        {
+            var path = PathToFile(filename);
+            using (StreamReader sr = File.OpenText(path))
+                return await sr.ReadToEndAsync();
+        }
+
+        public async Task SaveTextAsync(string fileName, string text)
+        {
+            var path = PathToFile(fileName);
+            using (StreamWriter sw = File.CreateText(path))
+                await sw.WriteAsync(text);
+        }
+        #endregion
+
+        #region CLASS_METHODS
+        /// <summary>
+        /// Return the path to a file
+        /// </summary>
+        /// <param name="filename">Name of the file</param>
+        /// <returns></returns>
+        private string PathToFile(string filename)
+        {
+            var docsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal);
+            return System.IO.Path.Combine(docsPath, filename);
+        }
+        #endregion
+    }
+}
---
+++ TrailMakers/TrailMakers/Interface/ISaveAndLoad.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrailMakers.Interface
+{
+    public interface ISaveAndLoad
+    {
+        /// <summary>
+        /// Save a text in a file
+        /// </summary>
+        /// <param name="fileName">Name for the file</param>
+        /// <param name="text"></param>
+        /// <returns></returns>
+        Task SaveTextAsync(string fileName, string text);
+        /// <summary>
+        /// Loads the text that is in a File
+        /// </summary>
+        /// <param name="filename">Name of the file</param>
+        /// <returns></returns>
+        Task<string> LoadTextAsync(string filename);
+        /// <summary>
+        /// Check if a file exists
+        /// </summary>
+        /// <param name="filename">Name of the file</param>
+        /// <returns>return True if the file exists and False if not</returns>
+        bool CheckFile(string filename);
+
+    }
+}
